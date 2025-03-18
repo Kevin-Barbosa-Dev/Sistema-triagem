d11ac023bc5873f5639290afb5c65361a19a9c8e
Victor_Alves <164104733+VictorGabriel-Dev@users.noreply.github.com>
2025-03-18 19:21:13 -0300

    Delete src directory

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import controller.CartaoController;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        CartaoController controller = new CartaoController();
-        controller.start();
-    }
-}
